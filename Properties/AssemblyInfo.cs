84d216ac604a0ed2b8c4dfdb06d39e4a59718303
Romain Ferraton <rferraton@users.noreply.github.com>
2022-07-08 15:02:58 +0200

    v1.0.0.0

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// Les informations générales relatives à un assembly dépendent du
+// jeu d'attributs suivant. Modifiez ces valeurs d'attribut pour modifier les informations
+// associées avec l'assembly SQLCLR.
+[assembly: AssemblyTitle("SQL CLR Hash Functions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Architecture & Performance")]
+[assembly: AssemblyProduct("SQLHASH")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Les informations de version pour un assembly incluent les quatre valeurs suivantes :
+//
+//      Version majeure
+//      Version mineure 
+//     Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- SQLDataHashFunctions.cs
+++ SQLDataHashFunctions.cs
@@ -73,45 +73,45 @@ namespace SQLDataHashFunctions
     public partial class MURMUR
     {
 
-       /***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is HashTableHashing.SuperFastHash.
- *
- * The Initial Developer of the Original MurmurHash2 Code is
- * Davy Landman.
- * Portions created by the Initial Developer are Copyright (C) 2009
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * Thomas Kejser - turning this code into SQL Server CLR version 
- *                 and adding MurmurHash3 implementation based on C++ source
- * 
- * Romain Ferraton - corrections
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+           /***** BEGIN LICENSE BLOCK *****
+     * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+     *
+     * The contents of this file are subject to the Mozilla Public License Version
+     * 1.1 (the "License"); you may not use this file except in compliance with
+     * the License. You may obtain a copy of the License at
+     * http://www.mozilla.org/MPL/
+     *
+     * Software distributed under the License is distributed on an "AS IS" basis,
+     * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+     * for the specific language governing rights and limitations under the
+     * License.
+     *
+     * The Original Code is HashTableHashing.SuperFastHash.
+     *
+     * The Initial Developer of the Original MurmurHash2 Code is
+     * Davy Landman.
+     * Portions created by the Initial Developer are Copyright (C) 2009
+     * the Initial Developer. All Rights Reserved.
+     *
+     * Contributor(s):
+     * Thomas Kejser - turning this code into SQL Server CLR version 
+     *                 and adding MurmurHash3 implementation based on C++ source
+     * 
+     * Romain Ferraton - corrections
+     *
+     * Alternatively, the contents of this file may be used under the terms of
+     * either the GNU General Public License Version 2 or later (the "GPL"), or
+     * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+     * in which case the provisions of the GPL or the LGPL are applicable instead
+     * of those above. If you wish to allow use of your version of this file only
+     * under the terms of either the GPL or the LGPL, and not to allow others to
+     * use your version of this file under the terms of the MPL, indicate your
+     * decision by deleting the provisions above and replace them with the notice
+     * and other provisions required by the GPL or the LGPL. If you do not delete
+     * the provisions above, a recipient may use your version of this file under
+     * the terms of any one of the MPL, the GPL or the LGPL.
+     *
+     * ***** END LICENSE BLOCK ***** */
  
  
         const UInt32 seed = 42; /* Define your own seed here */
@@ -129,7 +129,7 @@ namespace SQLDataHashFunctions
             UInt32 h = seed ^ (UInt32)length;
             Int32 currentIndex = 0;
             while (length >= 4)
-    {
+            {
                 UInt32 k = (UInt32)(data[currentIndex++]
                   | data[currentIndex++] << 8
                   | data[currentIndex++] << 16
@@ -273,6 +273,4 @@ namespace SQLDataHashFunctions
 
 };
 
-
-    }
 
